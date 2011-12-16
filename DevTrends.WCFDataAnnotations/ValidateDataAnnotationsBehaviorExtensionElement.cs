bd9b273370c08cdaaab0d1ed51550a723c05fc0c
DevTrends <none@none.com>
2011-12-16 16:11:08 +0000

    Added ability to add the behavior to a service via configuration

---
+++ DevTrends.WCFDataAnnotations/ValidateDataAnnotationsBehaviorExtensionElement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ServiceModel.Configuration;
+
+namespace DevTrends.WCFDataAnnotations
+{
+    public class ValidateDataAnnotationsBehaviorExtensionElement : BehaviorExtensionElement
+    {
+        public override Type BehaviorType
+        {
+            get { return typeof(ValidateDataAnnotationsBehavior); }
+        }
+
+        protected override object CreateBehavior()
+        {
+            return new ValidateDataAnnotationsBehavior();
+        }
+    }
+}
