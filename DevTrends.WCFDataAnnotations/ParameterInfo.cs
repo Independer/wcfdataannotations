628330fbc9a3ef0da532c4a979c967559f109fb4
Istvan Nagy <inagy@independer.nl>
2018-01-29 17:08:06 +0100

    Added optional SkipNullCheck attribute

---
+++ DevTrends.WCFDataAnnotations/ParameterInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DevTrends.WCFDataAnnotations {
+  public class ParameterInfo {
+    public List<ParameterDetails> ParameterDetails { get; set; }
+
+    public bool HasAnyParameterSkipNullCheck {
+      get { return ParameterDetails.Any(x => x.SkipNullcheck); }
+    }
+
+    public ParameterInfo() {
+      ParameterDetails = new List<ParameterDetails>();
+    }
+  }
+}
--- DevTrends.WCFDataAnnotations/ValidateDataAnnotationsBehavior.cs
+++ DevTrends.WCFDataAnnotations/ValidateDataAnnotationsBehavior.cs
@@ -1,84 +1,114 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
 using System.ServiceModel.Description;
 using System.ServiceModel.Dispatcher;
 
-namespace DevTrends.WCFDataAnnotations
-{
+namespace DevTrends.WCFDataAnnotations {
+  /// <summary>
+  /// Validates inputs using Data Annotations
+  /// </summary>
+  [AttributeUsage(AttributeTargets.Class)]
+  public class ValidateDataAnnotationsBehavior : Attribute, IServiceBehavior {
     /// <summary>
-    /// Validates inputs using Data Annotations
+    ///     The default _validating parameter inspector
     /// </summary>
-    [AttributeUsage(AttributeTargets.Class)]
-    public class ValidateDataAnnotationsBehavior : Attribute, IServiceBehavior
-    {
-        /// <summary>
-        ///     The _validating parameter inspector
-        /// </summary>
-        private readonly IParameterInspector _validatingParameterInspector;
+    private readonly IParameterInspector _defaultValidatingParameterInspector;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ValidateDataAnnotationsBehavior" /> class.
-        /// </summary>
-        public ValidateDataAnnotationsBehavior()
-        {
-            var validators = new IObjectValidator[]
-            {
-                new NullCheckObjectValidator(),
-                new DataAnnotationsObjectValidator(),
-                new ValidatableObjectValidator()
-            };
+    /// <summary>
+    ///     The list of _validators
+    /// </summary>
+    private readonly List<IObjectValidator> _validators;
 
-            var errorMessageGenerator = new ErrorMessageGenerator();
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ValidateDataAnnotationsBehavior" /> class.
+    /// </summary>
+    public ValidateDataAnnotationsBehavior() {
+      _validators = new List<IObjectValidator>
+      {
+        new NullCheckObjectValidator(),
+        new DataAnnotationsObjectValidator(),
+        new ValidatableObjectValidator()
+      };
 
-            _validatingParameterInspector = new ValidatingParameterInspector(validators, errorMessageGenerator);
-        }
+      var errorMessageGenerator = new ErrorMessageGenerator();
 
-        /// <summary>
-        ///     Provides the ability to pass custom data to binding elements to support the contract implementation.
-        /// </summary>
-        /// <param name="serviceDescription">The service description of the service.</param>
-        /// <param name="serviceHostBase">The host of the service.</param>
-        /// <param name="endpoints">The service endpoints.</param>
-        /// <param name="bindingParameters">Custom objects to which binding elements have access.</param>
-        public void AddBindingParameters(
+      _defaultValidatingParameterInspector = new ValidatingParameterInspector(_validators, errorMessageGenerator);
+    }
+
+    /// <summary>
+    ///     Provides the ability to pass custom data to binding elements to support the contract implementation.
+    /// </summary>
+    /// <param name="serviceDescription">The service description of the service.</param>
+    /// <param name="serviceHostBase">The host of the service.</param>
+    /// <param name="endpoints">The service endpoints.</param>
+    /// <param name="bindingParameters">Custom objects to which binding elements have access.</param>
+    public void AddBindingParameters(
             ServiceDescription serviceDescription,
             ServiceHostBase serviceHostBase,
             Collection<ServiceEndpoint> endpoints,
-            BindingParameterCollection bindingParameters)
-        {
-        }
+            BindingParameterCollection bindingParameters) { }
 
-        /// <summary>
-        ///     Provides the ability to change run-time property values or insert custom extension objects such as error handlers,
-        ///     message or parameter interceptors, security extensions, and other custom extension objects.
-        /// </summary>
-        /// <param name="serviceDescription">The service description.</param>
-        /// <param name="serviceHostBase">The host that is currently being built.</param>
-        public void ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
-        {
-            var operations =
-                from dispatcher in serviceHostBase.ChannelDispatchers.Cast<ChannelDispatcher>()
-                from endpoint in dispatcher.Endpoints
-                from operation in endpoint.DispatchRuntime.Operations
-                select operation;
+    /// <summary>
+    ///     Provides the ability to change run-time property values or insert custom extension objects such as error handlers,
+    ///     message or parameter interceptors, security extensions, and other custom extension objects.
+    /// </summary>
+    /// <param name="serviceDescription">The service description.</param>
+    /// <param name="serviceHostBase">The host that is currently being built.</param>
+    public void ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase) {
+      var operations =
+        from dispatcher in serviceHostBase.ChannelDispatchers.Cast<ChannelDispatcher>()
+        from endpoint in dispatcher.Endpoints
+        from operation in endpoint.DispatchRuntime.Operations
+        select operation;
 
-            foreach (var operation in operations)
-            {
-                operation.ParameterInspectors.Add(_validatingParameterInspector);
-            }
-        }
+      var contractMethods = serviceHostBase.Description.Endpoints.SelectMany(x => x.Contract.ContractType.GetMethods()).ToList();
 
-        /// <summary>
-        ///     Provides the ability to inspect the service host and the service description to confirm that the service can run
-        ///     successfully.
-        /// </summary>
-        /// <param name="serviceDescription">The service description.</param>
-        /// <param name="serviceHostBase">The service host that is currently being constructed.</param>
-        public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
-        {
-        }
+      foreach (var operation in operations) {
+        var parameterInfo = GetParameterInfo(operation.Name, contractMethods);
+
+        operation.ParameterInspectors.Add(parameterInfo.HasAnyParameterSkipNullCheck
+          ? new ValidatingParameterInspector(_validators, new ErrorMessageGenerator(), parameterInfo)
+          : _defaultValidatingParameterInspector);
+      }
     }
+
+    /// <summary>
+    ///     Provides the ability to inspect the service host and the service description to confirm that the service can run
+    ///     successfully.
+    /// </summary>
+    /// <param name="serviceDescription">The service description.</param>
+    /// <param name="serviceHostBase">The service host that is currently being constructed.</param>
+    public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase) { }
+
+    /// <summary>
+    /// Gets the parameters information for the given operation
+    /// </summary>
+    /// <param name="operationName"></param>
+    /// <param name="contractMethods"></param>
+    /// <returns></returns>
+    private ParameterInfo GetParameterInfo(string operationName, IEnumerable<MethodInfo> contractMethods) {
+      var parameterInfo = new ParameterInfo();
+
+      var parameters = contractMethods.Single(x => x.Name == operationName).GetParameters();
+      
+      foreach (var parameter in parameters.OrderBy(x => x.Position)) {
+        var skipNullCheck = false;
+
+        foreach (var customAttribute in parameter.GetCustomAttributes(inherit: false)) {
+          if (customAttribute is SkipNullCheckAttribute) {
+            skipNullCheck = true;
+          }
+        }
+
+        parameterInfo.ParameterDetails.Add(new ParameterDetails { Name = parameter.Name, Position = parameter.Position, SkipNullcheck = skipNullCheck });
+      }
+
+      return parameterInfo;
+    }
+  }
 }
